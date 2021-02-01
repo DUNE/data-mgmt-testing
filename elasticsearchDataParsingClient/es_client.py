81a39ec82c6f7cd08948bced3274678b96f1e0ce
Zachary Lee <zacharyianlee@gmail.com>
2021-02-01 11:56:30 -0600

    Switched to using search function instead of search_template (to actually search based on a template instead of searching FOR a template). Still need to figure out date ranges.

--- elasticsearchDataParsingClient/es_client.py
+++ elasticsearchDataParsingClient/es_client.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 import sys
+import json
+import pprint
 from elasticsearch import Elasticsearch
 
+printer = pprint.PrettyPrinter(indent=4)
 
 y,m,d = sys.argv[1].split('/')
 
@@ -14,26 +17,28 @@ client = Elasticsearch([es_cluster])
 
 es_template = {
     "query" : {
-        "range" : {
-            "@timestamp" : {
-                "gt" : f"{y}-{m}-{d}, 00:00:00",
-                "lte" : f"{y}-{m}-{d}, 23:59:59"
-            }
-        },
+   #     "range" : {
+    #        "@timestamp" : {
+     #           "gt" : f"{y}-{m}-{d}, 00:00:00",
+    #            "lte" : f"{y}-{m}-{d}, 23:59:59"
+     #       }
+      #  },
         "match": {
             "event_type" : "transfer-done"
         }
     }
 }
 
-res = client.search_template(index = index, body = es_template)
+res = client.search(index = index, body=es_template)
 
 output_string = ""
 
 #Formats for the previously made JSON parser
-for result in res:
-    output_string += str(result) + "\n"
+#for result in res:
+#    output_string += str(result) + "\n"
+
+jres = json.dumps({"data": res}, indent=2)
 
 f = open(output_file, "w+")
-f.write(output_string)
+f.write(jres)
 f.close()
