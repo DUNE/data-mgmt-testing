4a475ab6c17daae187c95799334efc596b00a889
Heidi Schellman <heidi.schellman@oregonstate.edu>
2021-02-19 08:29:50 -0800

    fix types list code

--- XrootParser/ByDay.py
+++ XrootParser/ByDay.py
@@ -2,12 +2,13 @@ import Utils
 import datetime
 
 
-start_date = datetime.date(2021, 2, 8)
+start_date = datetime.date(2021, 1, 1)
 start_range = start_date
-end_date = datetime.date(2021, 2, 9)
+end_date = datetime.date(2021, 1, 10)
 delta = datetime.timedelta(days=1)
 while start_range <= end_date:
     
     end_range = start_range + delta
-    Utils.test(start_range,end_range,1)
+    Utils.test(start_range,end_range,100000)
     start_range += delta
+
