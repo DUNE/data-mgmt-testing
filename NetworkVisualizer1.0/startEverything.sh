308770266c95eb6c2d6fa2849c906948b8c54eea
cathulhu <cathulhu@users.noreply.github.com>
2021-05-28 15:06:32 -0700

    Update startEverything.sh
    Updated script text to reflect new duration

--- NetworkVisualizer1.0/startEverything.sh
+++ NetworkVisualizer1.0/startEverything.sh
@@ -3,4 +3,7 @@ forever --sourceDir "api"  start -c "npm start" ./
 forever start -c "npm start" ./
 echo ""
 echo ""
-echo "Ok a browser window should pop up shortly with the interface"
+echo "Ok if that worked the server should be up in <10 minutes, if successful a browser window should pop up shortly with the interface, assuming you have X-window forward on, or or running in a GUI."
+echo ""
+echo ""
+
