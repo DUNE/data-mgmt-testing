f7caf4efeefa9ff90795b123a08447f3b3ff6de6
cathulhu <cathulhu@users.noreply.github.com>
2021-06-03 15:10:29 -0700

    Update setupEverything.sh
    minor correction to setup script line breaks.

--- NetworkVisualizer1.0/setupEverything.sh
+++ NetworkVisualizer1.0/setupEverything.sh
@@ -2,9 +2,7 @@
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+export NVM_DIR="$HOME/.nvm" [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 nvm install node
 echo ""
