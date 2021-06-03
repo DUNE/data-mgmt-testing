3c79e2d133d7ba0b634d08aa4c1204c2c485f9e4
cathulhu <brynmool@fermicloud499.fnal.gov>
2021-06-03 22:58:00 +0000

    fix for not ebing able to export nvm var to .bashrc

--- NetworkVisualizer1.0/stopEverything.sh
+++ NetworkVisualizer1.0/stopEverything.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+export NVM_DIR="$HOME/.nvm" 
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
 forever stopall
 
 fuser -k -n tcp 3000
