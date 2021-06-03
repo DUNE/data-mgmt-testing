48b1dcf0ab2d6507c873276559d9357c10bf2a5a
cathulhu <cathulhu@users.noreply.github.com>
2021-06-03 15:36:26 -0700

    Update startEverything.sh
    added nvm init to start script

--- NetworkVisualizer1.0/startEverything.sh
+++ NetworkVisualizer1.0/startEverything.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+export NVM_DIR="$HOME/.nvm" 
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
 forever --sourceDir "api"  start -c "npm start" ./
 forever start -c "npm start" ./
 echo ""
