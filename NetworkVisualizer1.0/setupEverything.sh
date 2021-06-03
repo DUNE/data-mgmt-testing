5080885688557bb1fdd376388d13d91129e63841
Lydia Brynmoor <brynmool@fermicloud499.fnal.gov>
2021-06-03 20:29:09 +0000

    updated setup script for nvm node install

--- NetworkVisualizer1.0/setupEverything.sh
+++ NetworkVisualizer1.0/setupEverything.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+nvm install node
+node --version
+npm --version
+
+
 npm install --prefix
 echo ""
 echo ""
