05e538bd73c6fa6646437bf3150fdad8eb7026c9
Zachary Lee <zacharyianlee@gmail.com>
2021-07-29 02:59:00 -0700

    Removed json clutter. Removed directories unrelated to this specific branch

--- R_Code/README.md
+++
@@ -1,3 +0,0 @@
-### This folder contains R Code for data visualization and analysis
-### Created by Sean Gilligan
-### With supervision by Heidi Schellman 
--- XrootParser/conda_setup.sh
+++
@@ -1,6 +0,0 @@
-export APP=/dune/app/users/$USER
-source $APP/miniconda/etc/profile.d/conda.sh
-export DBLUE=/dune/data/users/$USER
-export XROOT=$DBLUE/data-mgmt-testing/XrootParser
-conda activate my_root_env
-export PYTHONPATH=$APP/sam-web-client/python:${PYTHONPATH}
